b1601617e37d6bcc166d3833b6fe967893d630f4
aji529 <aji529@gmail.com>
2020-12-16 22:33:32 +0530

    Initial version of calculator algorithm, parent check in.

---
+++ src/package1/MainClass.java
@@ -0,0 +1,16 @@
+package package1;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("-----------CALCULATOR----------");
+
+		Operations first = new Operations();
+		
+		first.scaninput();
+		first.PerformOP();
+
+	}
+
+}
