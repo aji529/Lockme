cfdad428e4931eda64c62626ee217f2d6b669423
aji529 <aji529@gmail.com>
2021-01-20 00:38:22 +0530

    Code refinement and changes maded to File Handling, and additional function added to main list

--- Lockme.com/src/com/lockme/app/OperationsCall.java
+++ Lockme.com/src/com/lockme/app/OperationsCall.java
@@ -13,31 +13,31 @@ public class OperationsCall {
 	static Scanner name;
 	static FileHandler fh;
 
-	
-	
 	/**
 	 * This function accepts user input for root directory and exits on providing, a
 	 * valid directory path.
 	 */
 	public void getroot() {
-		System.out.println("Please Enter the root directory");
+		System.out.println("Please Enter the root directory:");
 		while (true) {
+			
 			String rootpath = readStringInput();
 			File file = new File(rootpath);
+			
 			if (file.isDirectory()) {
+				
 				System.out.println("Root Path Accepted : " + file.getAbsolutePath());
 				this.rootpath = rootpath;
 				break;
 
 			} else {
-				System.out.println("Invalid directory path, please enter the correct rootpath");
+				
+				System.err.println("Invalid directory path, please enter the correct rootpath");
+				
 			}
 		}
 	}
 
-	
-	
-	
 	/**
 	 * This reads the options for selecting choice, reads integer options for both
 	 * main and sub-level functions.
@@ -45,23 +45,34 @@ public class OperationsCall {
 	 * @return integer option selected.
 	 */
 	public static int readInput() {
+		
 		input = new Scanner(System.in);
 		int choice;
-		if (input.hasNext()) {
-			try {
-				choice = input.nextInt();
-			} catch (InputMismatchException m) {
-				System.out.println("Enter valid input");
-				choice = 1;
+		
+		while (true) {
+			
+			while (!input.hasNextInt()) {
+				
+				input.nextLine(); // Discard junk entries
+				System.err.println("...Invalid input, please enter a valid input: ");
+				
 			}
-		} else {
-			choice = 1;
+			try {
+				
+				choice = input.nextInt();
+				break;
+				
+			} catch (InputMismatchException m) {
+				
+				System.err.println("...Invalid input, please enter a valid input:");
+				
+			}
+			
 		}
+		
 		return choice;
 	}
 
-	
-	
 	/**
 	 * Accepts string input, for directory path and file name as string from user
 	 * 
@@ -69,31 +80,15 @@ public class OperationsCall {
 	 */
 	public static String readStringInput() {
 		name = new Scanner(System.in);
-		String file;
+		String inString;
 		if (name.hasNext()) {
-			file = name.nextLine();
+			inString = name.nextLine();
 		} else {
-			file = null;
+			inString = null;
 		}
-		return file;
-	}
-	
-
-	
-	public void mainOptions() {
-		System.out.println("***********************************************");
-		System.out.println("Select from the below list of operations: ");
-		System.out.println("***********************************************");
-		System.out.println("1.Retrieve the file names in an ascending order");
-		System.out.println("2.Business-level operations:");
-		System.out.println("3.Close the aplication");
-		System.out.println("***********************************************");
-
+		return inString;
 	}
 
-	
-	
-	
 	/**
 	 * Calls function corresponding to user choice, perform the action chosen by the
 	 * user.
@@ -101,47 +96,61 @@ public class OperationsCall {
 	 * @param choice
 	 */
 	public void parentOptions() {
-		System.out.println("***********************************************");
+		
+		System.out.println("************************************************");
 		System.out.println("Select from the below list of operations: ");
-		System.out.println("***********************************************");
-		System.out.println("1.Retrieve the file names in an ascending order");
-		System.out.println("2.Business-level operations:");
-		System.out.println("3.Close the aplication");
-		System.out.println("***********************************************");
+		System.out.println("************************************************");
+		System.out.println("1.Retrieve the file names in an ascending order.");
+		System.out.println("2.Business-level operations.");
+		System.out.println("3.Change the root path.");
+		System.out.println("4.Close the aplication.");
+		System.out.println("************************************************");
 		int choice = readInput();
+		
 		switch (choice) {
+		
 		case 1:
+			
 			// Display file to in ascending order:
 			fh = new FileHandler(rootpath);
 			fh.sortFile();
 			break;
-
+			
 		case 2:
+			
 			// Displays the sub-level feature.
 			secondaryOptions();
 			break;
 
 		case 3:
 			
+			this.getroot();
+			break;
+
+		case 4:
+			
 			input.close();
 			name.close();
-			System.out.println("\\t\" + \"\\t\" + \"\\t\" + \"\\t\" + \"************LOCKME.COM APPLICATION CLOSED************\" + \"\\t");
+			System.out.println(
+					"\t" + "\t" + "\t" + "\t" + "************LOCKME.COM APPLICATION CLOSED************" + "\t");
 			System.exit(0);
 			break;
+			
 		default:
-			System.out.println("Please enter a valid choice");
+			
+			System.err.println("...invalid choice.");
 			break;
+			
 		}
 	}
 
-	
-	
 	/**
 	 * Calls sub-level functions as per the input of the user. User must choose by
 	 * typing in the integer value against the desired operation.
 	 */
 	public void secondaryOptions() {
-		boolean loop=true;
+		
+		boolean loop = true;
 		while (loop) {
 			
 			System.out.println("*********************************************");
@@ -152,13 +161,14 @@ public class OperationsCall {
 			System.out.println("3.Search a file from the main directory");
 			System.out.println("4.Navigate back to the main context");
 			System.out.println("*********************************************");
-
 			String filename;
 			int choice = readInput();
 
 			// switch logic to choose operation according to choice.
 			switch (choice) {
+			
 			case 1:
+				
 				System.out.println("-------------------");
 				System.out.println("Options 1 selected");
 				System.out.println("-------------------");
@@ -167,7 +177,9 @@ public class OperationsCall {
 				fh = new FileHandler(rootpath);
 				fh.addFile(filename);
 				break;
+				
 			case 2:
+				
 				System.out.println("-------------------");
 				System.out.println("Options 2 selected");
 				System.out.println("-------------------");
@@ -176,73 +188,95 @@ public class OperationsCall {
 				fh = new FileHandler(rootpath);
 				fh.deleteFile(filename, rootpath);
 				break;
+				
 			case 3:
+				
 				System.out.println("-------------------");
 				System.out.println("Options 3 selected");
 				System.out.println("-------------------");
 				System.out.println("Please enter the type of search from the below options:");
 				SearchType type = searchMode();
 				boolean subSearch = subSearch();
-				System.out.println("Enter Search Phrase or regex : ");
+				System.out.println("Enter Search Phrase or extension: ");
 				filename = readStringInput();
 				fh = new FileHandler(rootpath);
-				fh.searchFile(filename,type,this.rootpath, subSearch);
+				fh.searchFile(filename, type, this.rootpath, subSearch);
 				break;
+				
 			case 4:
+				
 				System.out.println("-----------------------------------------------------");
 				System.out.println("Options 4 selected, Navaigating back to the main menu");
 				System.out.println("-----------------------------------------------------");
-				loop= false;
+				loop = false;
 				break;
+				
 			default:
+				
 				System.out.println("Please choose from the given options");
 				break;
+				
 			}
+			
 		}
+		
 	}
 
-	
-	
-	
-	
 	/**
-	 * Asks whether user prefers a top level search, a detailed search that searches the sub-folder.
+	 * Asks whether user prefers a top level search, a detailed search that searches
+	 * the sub-folder.
+	 * 
 	 * @return boolean value.
 	 */
 	public boolean subSearch() {
-		System.out.println("Do you wish to search the sub-folders for matches? Type true or false :");
-		boolean sub = false; 
-		if(readStringInput().toLowerCase().contentEquals("true")) {
+		
+		System.out.println("Do you wish to search the sub-folders for matches? Type true or false (default set to false) :");
+		
+		boolean sub = false;
+		if (readStringInput().toLowerCase().contentEquals("true")) {
+			
 			sub = true;
+			
 		}
 		return sub;
 	}
 
 	
 	
-	
-	
 	/**
-	 * This method allows the user to choose from type of search.
-	 * "EXACT        --search returns result that is an exact match
-	 * "CONTAINS     --search returns file that contains search value
-	`* "STARTS_WITH  --returns files starting with search phrase");
-	 * "BY_EXTENSION -- returns file of the extension searched
-	 * @return type selected by user, in case of illegal argument type passed is EXACT.
+	 * This method allows the user to choose from type of search. "EXACT --search
+	 * returns result that is an exact match "CONTAINS --search returns file that
+	 * contains search value `* "STARTS_WITH --returns files starting with search
+	 * phrase"); "BY_EXTENSION -- returns file of the extension searched
+	 * 
+	 * @return type selected by user, in case of illegal argument type passed is
+	 *         EXACT.
 	 */
 	public SearchType searchMode() {
-		System.out.println("EXACT        --search returns result that is an exact match");
-		System.out.println("CONTAINS     --search returns file that contains search value ");
-		System.out.println("STARTS_WITH  --returns files starting with search phrase");
-		System.out.println("BY_EXTENSION --returns file of the extension searched");
+
+		System.out.println("EXACT        --search returns file that is an exact match to search value.");
+		System.out.println("CONTAINS     --search returns file that contains search value.");
+		System.out.println("STARTS_WITH  --search returns files starting with search phrase.");
+		System.out.println("BY_EXTENSION --search returns file of the extension searched,");
 		SearchType type;
-		try {
-			 type = SearchType.valueOf(readStringInput().toUpperCase());
+		
+		while (true) {
+			
+			try {
+				
+				type = SearchType.valueOf(readStringInput().toUpperCase());
+				return type;
+				
+			} catch (IllegalArgumentException e) {
+				
+				System.err.println("..invalid search type entered, Choose from the given option.");
+				
+			}
+
 		}
-		catch(IllegalArgumentException e) {
-			System.out.println("The Search type entered is not valid.");
-			type = SearchType.EXACT;
-		}
-		return type;
+
 	}
+	
+	
+	
 }
--- Lockme.com/src/com/lockme/filehandler/FileHandler.java
+++ Lockme.com/src/com/lockme/filehandler/FileHandler.java
@@ -10,46 +10,56 @@ import java.util.Scanner;
 
 public class FileHandler {
 	String rootpath = null;
-	
+
 	public FileHandler(String rootpath) {
+
 		this.rootpath = rootpath;
+
 	}
 
-	
-	
-	
 	/**
 	 * This method generates a list of contents of a directory and displays it in
 	 * sorted order.
 	 */
 	public void sortFile() {
+
 		File file = new File(rootpath);
-		File[] dircnt = file.listFiles();
-		List<String> dircontent = new ArrayList<String>();
-		if (dircnt == null) {
-			System.out.println("The given directory is empty");
+		if (file.canRead()) {
+
+			File[] dircnt = file.listFiles();
+			List<String> dircontent = new ArrayList<String>();
+			if (dircnt == null) {
+
+				System.out.println("The given directory is empty");
+
+			} else {
+
+				for (File f : dircnt) {
+
+					String fname = f.getName();
+					dircontent.add(fname);
+
+				}
+				dircontent.sort(String::compareToIgnoreCase);
+				dircontent.forEach((cnt) -> {
+					String append = " --> is  a Directory";
+					if ((new File(rootpath + "\\" + cnt)).isDirectory()) {
+						System.out.println(cnt + append);
+					} else {
+						System.out.println(cnt);
+					}
+				});
+
+			}
+
 		} else {
 
-			for (File f : dircnt) {
-				String fname = f.getName();
-				dircontent.add(fname);
-			}
-			dircontent.sort(String::compareToIgnoreCase);
-			dircontent.forEach((cnt) -> {
-				String append = " --> is  a Directory";
-				if ((new File(rootpath + "\\" + cnt)).isDirectory()) {
-					System.out.println(cnt + append);
-				} else {
-					System.out.println(cnt);
-				}
-			});
+			System.out.println("File specified by the root path cannot be read.");
+
 		}
 
 	}
 
-	
-	
-	
 	/**
 	 * Adds a file of the given name to the directory pointed towards by the root
 	 * path.
@@ -60,17 +70,26 @@ public class FileHandler {
 
 		File file = new File(rootpath + '\\' + filename);
 		try {
-			file.createNewFile();
-			System.out.println("File " + filename + " created in the directory " + this.rootpath);
+			if (file.exists()) {
+
+				System.err.println(file.getName() + " already exists in the root directory");
+
+			} else {
+
+				file.createNewFile();
+				System.out.println("File " + filename + " created in the directory " + this.rootpath);
+
+			}
+
 		} catch (IOException e) {
-			System.out.println("File could not be created");
+
+			System.err.println("File could not be created");
 			e.printStackTrace();
+
 		}
+
 	}
 
-	
-	
-	
 	/**
 	 * Function deletes a file passed by the user. If a file is a Directory it
 	 * further checks its content and call function confirmDelete().
@@ -78,35 +97,61 @@ public class FileHandler {
 	 * @param filename
 	 */
 	public void deleteFile(String filename, String rootpath) {
+
 		File file = new File(rootpath + '\\' + filename);
-
 		if (file.exists()) {
-			if (file.isDirectory()) {
-				confirmDelete(file);
-			} else {
-				file.delete();
-				System.out.println("File " + filename + " deleted from root directory " + rootpath);
-			}
 
+			if (file.isDirectory()) {
+
+				confirmDelete(file);
+
+			} else {
+
+				if (file.canWrite()) {
+
+					file.delete();
+					System.out.println("File " + filename + " deleted from root directory " + rootpath);
+
+				} else {
+
+					System.err.println("Permission denied for modifying " + filename
+							+ " ... Delete operation could not be performed ");
+
+				}
+			}
 		} else {
-			System.out.println("File " + filename + " do not exit in the root directory " + rootpath);
+
+			System.err.println("File " + filename + " do not exit in the root directory " + rootpath);
+
 		}
+
 	}
 
-	
-	
 	/**
 	 * Handles deleting a file based on permission from a user, if its is directory
-	 * with contents and iterates in itself to dlete all sub-folders and file.
+	 * with contents and iterates in itself to delete all sub-folders and file.
 	 * 
 	 * @param file
 	 */
 	@SuppressWarnings("resource")
 	private void confirmDelete(File file) {
+
 		File[] allContents = file.listFiles();
-		if (allContents == null) {
-			file.delete();
+		if (allContents.length == 0) {
+
+			if (file.canWrite()) {
+
+				file.delete();
+				System.out.println("File Deleted : " + file.getAbsolutePath());
+
+			} else {
+
+				System.err.println("Permission denied for modifiying " + file.getName() + "...Delete operation could not be performed");
+
+			}
+
 		} else {
+
 			Scanner in = new Scanner(System.in);
 			String per;
 			ArrayList<String> ar1 = new ArrayList<>(Arrays.asList(file.list()));
@@ -114,31 +159,43 @@ public class FileHandler {
 			System.out.println("The directory has the below contents :");
 
 			while (itr.hasNext()) {
+
 				System.out.println(itr.next());
+
 			}
 
 			System.out.println("Are you sure you want to delete the folder, press 'Y' to delete and 'N' to not delete");
-			per = in.next("[ny]");
+
 			while (true) {
+
+				per = in.next();
 				if (per.equalsIgnoreCase("N")) {
+
 					System.out.println("File not deleted");
 					break;
+
 				} else if (per.equalsIgnoreCase("Y")) {
+
 					for (File f1 : allContents) {
 						deleteFile(f1.getName(), f1.getParent());
-					}
-					deleteFile(file.getName(), file.getParent());
 
+					}
+
+					deleteFile(file.getName(), file.getParent());
 					break;
+
 				} else {
-					System.out.println("Enter either Y/N to give permission");
+
+					System.out.println("Enter either Y/N to grant permission: ");
+
 				}
+
 			}
+
 		}
 
 	}
 
-		
 	/**
 	 * Search a file in a directory and its sub-directories. Displays the absolute
 	 * path for the matching search results. E-num data type, searchType defines the
@@ -155,37 +212,57 @@ public class FileHandler {
 		File file = new File(rootpath);
 		File[] allContent = file.listFiles();
 		if (allContent != null && searchMode) {
+			
 			for (File inside : allContent) {
+				
 				if (inside.isDirectory())
 					searchResult = searchResult | searchFile(searchPhrase, type, inside.getAbsolutePath(), searchMode);
+				
 			}
 		}
+		
 		File[] content;
 		switch (type) {
+		
 		case EXACT:
 			content = (File[]) file.listFiles((dir, name) -> name.equals(searchPhrase));
 			break;
+			
 		case CONTAINS:
+			
 			content = (File[]) file.listFiles((dir, name) -> name.contains(searchPhrase));
 			break;
+			
 		case STARTS_WITH:
+			
 			content = (File[]) file.listFiles((dir, name) -> name.startsWith(searchPhrase));
 			break;
+			
 		case BY_EXTENSION:
+			
 			content = (File[]) file.listFiles((dir, name) -> name.endsWith(searchPhrase));
 			break;
+			
 		default:
+			
 			content = (File[]) file.listFiles((dir, name) -> name.contentEquals(searchPhrase));
 			break;
+			
 		}
 
 		if (content != null) {
+			
 			for (File matches : content) {
-				System.out.println("File Found in the given root directory :" + matches.getAbsolutePath());
+				
+				System.out.println("File found inside given root directory, with file path:" + matches.getAbsolutePath());
 				searchResult = true;
+				
 			}
+			
 		} else {
+			
 			searchResult = false;
+			
 		}
 
 		return searchResult;
